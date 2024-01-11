5ebe5d84b20a93602aa0c9515064de536244538f
elenapoddighe <podd.elena@gmail.com>
2024-01-11 14:48:47 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/elena-poddighe-736276104/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Elena Poddighe
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/elenapoddighe/my-awesome-weather-react-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/elena-poddighe-736276104/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Elena Poddighe
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/elenapoddighe/my-awesome-weather-react-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
