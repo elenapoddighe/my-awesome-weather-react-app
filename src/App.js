c41ceb8c5eba12f08bab0424a2d11d08afd8a7ef
elenapoddighe <podd.elena@gmail.com>
2024-01-11 14:38:52 +0100

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/elena-poddighe-736276104/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Elena Poddighe
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/elenapoddighe/my-awesome-weather-react-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced
+        </a>
+      </footer>
+    </div>
+  );
 }
